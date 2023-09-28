f613e81d5635a9e8495a50d9b3a029b89393f2a7
Andy-Dickinson <dickinson.andrew@hotmail.co.uk>
2023-09-28 15:44:48 +0100

    comments added to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # imports the Flask class from the flask library
+app = Flask(__name__) # Creates an instance of the Flask class. __name__ is the name of flask applications module. Variable 'app' is used to reference the instantiated Flask class
 
-@app.route('/')
-def hello_world():
+@app.route('/') # lines beginning @ are decorators, route() decorator here tells Flask which URL should trigger the function that it decorates. Here when browser hits root of url ('/')
+def hello_world(): 
     return 'Hello Napier'
